a6c0964b40fff4bd1a5617cb068119c058a08c07
Marcel Ludwig <london_marcel@hotmail.com>
2024-06-19 17:20:59 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/projetospring/course/repositories/OrderItemRepository.java
+++ src/main/java/com/projetospring/course/repositories/OrderItemRepository.java
@@ -3,6 +3,7 @@ package com.projetospring.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.projetospring.course.entities.OrderItem;
+import com.projetospring.course.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 }
