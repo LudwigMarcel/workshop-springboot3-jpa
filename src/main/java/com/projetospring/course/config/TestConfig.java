33cde31e1ad068cca6553dc2b95af5c65d1048c2
Marcel Ludwig <london_marcel@hotmail.com>
2024-06-19 21:27:57 -0300

    Payment, one-to-one association

--- src/main/java/com/projetospring/course/config/TestConfig.java
+++ src/main/java/com/projetospring/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projetospring.course.entities.Category;
 import com.projetospring.course.entities.Order;
 import com.projetospring.course.entities.OrderItem;
+import com.projetospring.course.entities.Payment;
 import com.projetospring.course.entities.Product;
 import com.projetospring.course.entities.User;
 import com.projetospring.course.entities.enums.OrderStatus;
@@ -77,6 +78,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.saveAll(Arrays.asList(o1));
+		
 		
 
 	}
