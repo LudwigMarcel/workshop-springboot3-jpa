cd3f5d32e7c9a2ea89596debff0b3df8ed6f043b
Marcel Ludwig <london_marcel@hotmail.com>
2024-06-20 16:14:09 -0300

    User Update

--- src/main/java/com/projetospring/course/services/UserService.java
+++ src/main/java/com/projetospring/course/services/UserService.java
@@ -14,8 +14,8 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
 
@@ -23,12 +23,24 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 }
